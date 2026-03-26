4707435018eb441fbb8b6ac39fb95a7072bf2b74
dicioz <cristian.dicillo@studenti.unimi.it>
2026-03-26 10:28:41 +0100

    Initial Commit

---
+++ PadelHits Watch App/PadelHitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelHitsApp.swift
+//  PadelHits Watch App
+//
+//  Created by Cristian Di Cillo on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PadelHits_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PadelHits/PadelHitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelHitsApp.swift
+//  PadelHits
+//
+//  Created by Cristian Di Cillo on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PadelHitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
